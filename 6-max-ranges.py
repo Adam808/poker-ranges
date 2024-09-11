44d7c9b9b0365a7cf99db58064606cf2ef817580
unknown <adamparker05@gmail.com>
2024-09-11 14:07:20 -0400

    update ranges

--- 6-max-ranges.py
+++ 6-max-ranges.py
@@ -22,35 +22,29 @@ for i in ranks[ranks.index('k'):]:
 for i in ranks[ranks.index('q'):]:
     btn.append('k' + i + 's')
 
-for i in ranks[ranks.index('j'): ranks.index('3')]:
+for i in ranks[ranks.index('j'):]:
     btn.append('q' + i + 's')
 
-for i in ranks[ranks.index('t'): ranks.index('5')]:
+for i in ranks[ranks.index('t'): ranks.index('3')]:
     btn.append('j' + i + 's')
 
 for i in ranks[ranks.index('9'): ranks.index('5')]:
     btn.append('t' + i + 's')
 
-for i in ranks[ranks.index('8'): ranks.index('4')]:
+for i in ranks[ranks.index('8'): ranks.index('5')]:
     btn.append('9' + i + 's')
 
-for i in ranks[ranks.index('7'): ranks.index('3')]:
+for i in ranks[ranks.index('7'): ranks.index('5')]:
     btn.append('8' + i + 's')
 
-for i in ranks[ranks.index('6'): ranks.index('3')]:
+for i in ranks[ranks.index('6'): ranks.index('4')]:
     btn.append('7' + i + 's')
 
-for i in ranks[ranks.index('5'): ranks.index('2')]:
-    btn.append('6' + i + 's')
-
-for i in ranks[ranks.index('4'): ranks.index('2')]:
-    btn.append('5' + i + 's')
-
 # unsuited
-for i in ranks[ranks.index('k'):]:
+for i in ranks[ranks.index('k'): ranks.index('2')]:
     btn.append('a' + i + 'o')
 
-for i in ranks[ranks.index('q'): ranks.index('8')]:
+for i in ranks[ranks.index('q'): ranks.index('7')]:
     btn.append('k' + i + 'o')
 
 for i in ranks[ranks.index('j'): ranks.index('8')]:
@@ -59,8 +53,11 @@ for i in ranks[ranks.index('j'): ranks.index('8')]:
 for i in ranks[ranks.index('t'): ranks.index('8')]:
     btn.append('j' + i + 'o')
 
+for i in ranks[ranks.index('9'): ranks.index('7')]:
+    btn.append('t' + i + 'o')
+
 # singles
-singles = ['43s', '32s', 't9o', '98o']
+singles = [ '65s', '54s']
 for i in singles:
     btn.append(i)
 
@@ -68,14 +65,14 @@ for i in singles:
 sb = list(btn)
 
 # co
-co = [i for i in btn if i not in ('k5s', 'k4s', 'k3s', 'k2s', 'q7s', 'q6s', 'q5s', 'q4s', 'j7s', 'j6s', 't7s', 't6s', '96s', '95s', '85s', '84s', '74s', '63s', '53s',
-                                 '43s', '32s', 'a9o', 'a8o', 'a7o', 'a6o', 'a5o', 'a4o', 'a3o', 'a2o', 'k9o', 'q9o', 'j9o', 't9o', '98o')]
+co = [i for i in btn if i not in ('33', '22', 'q4s', 'q3s', 'q2s', 'j6s', 'j5s', 'j4s', 't7s', 't6s', '96s', '86s', 
+                                  'a7o', 'a60', 'a4o', 'a3o', 'a2o', 'k9o', 'k8o', 'q9o', 'j9o', 't9o', 't8o')]
 
 # hj
-hj = [i for i in co if i not in ('k7s', 'k6s', 'q8s', 'j8s', '86s', '75s', '64s', 'kto', 'qto', 'jto')]
+hj = [i for i in co if i not in ('44', 'k4s', 'k3s', 'k2s', 'q7s', 'q6s', 'q5s', 'j8s', 'j7s', '97s', '87s', 'a8o', 'qto', 'jto', 'a5o')]
 
 # utg
-utg = [i for i in hj if i not in ('k8s', 't8s', '97s', '54s', 'ato', 'kjo', 'qjo')]
+utg = [i for i in hj if i not in ('k8s', 'a2s', 'q8s', 't8s', '98s', 'a9o', 'kto', 'qjo')]
 
 def play_hand():
     global btn, co, hj, utg, sb
@@ -88,7 +85,7 @@ def play_hand():
         print(hand in range_dict[position])
 
 play_hand()
-    
+
     
 
 
